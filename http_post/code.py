1bfe7e72601f4293679eab01eab9f76bf982435a
tamberg <tamberg@yaler.net>
2022-08-29 14:55:12 +0200

    Update.

--- http_post/code.py
+++ http_post/code.py
@@ -7,7 +7,7 @@ import adafruit_requests
 
 WIFI_SSID = "MY_SSID" # TODO
 WIFI_PASS = "MY_PASSWORD" # TODO
-CLOUD_KEY = "..." # TODO, ThingSpeak Write API Key
+CLOUD_KEY = "****************" # TODO, ThingSpeak Write API Key
 CLOUD_URL = "https://api.thingspeak.com/update.json"
 
 print("Connecting to Wi-Fi \"{0}\"...".format(WIFI_SSID))
@@ -21,10 +21,10 @@ https = adafruit_requests.Session(socket, context)
 while True:
     value = 23.0 # e.g. from sensor
     json_data = {
-        "api_key": CLOUD_KEY
+        "api_key": CLOUD_KEY,
         "field1": value, 
     }
-    print("{0}\n\n{1}".format(CLOUD_URL, json_data))
+    print("Posting to {0}\n> {1}".format(CLOUD_URL, json_data))
     response = https.post(CLOUD_URL, json=json_data)
-    print(response.json())
+    print("< {0}".format(response.json()))
     time.sleep(30) # s
